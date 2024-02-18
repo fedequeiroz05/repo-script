4232b86eb82fc2f71033f803b9d6d6bbdd2dfe26
Fededaher <felipequeiroz05@gmail.com>
2024-02-18 10:45:31 -0300

    first add the script

---
+++ AppCMDjobUtil.ps1
@@ -0,0 +1,21 @@
+
+#Script para executar o comando de instalacao de applicationpool no servidor IIS em modo paralelo.
+function Add-applicationPool {
+    param(
+        [string[]] $computername,
+        [string] $applicationPoolName
+    )
+
+    $session = $computername | ForEach-Object { New-PSSession -ComputerName $_}
+    $jobs = $session | ForEach-Object {
+        Invoke-Command -Session $_ -ScriptBlock {
+            $appCmdArgs = "add apppool /name:$($args[0]) /managedRunTimeVersion:v4.0 /managedPipelineMode:Integrated"
+        c:\windows\system32\inetsrv\appcmd.exe $appCmdArgs.Split(' ') 
+        } -ArgumentList $applicationPoolName -AsJob
+    }
+
+    $jobs | Wait-Job | Select-Object @{Expression={ receive-job $_ };Label="resultado"},"Name"
+    $jobs | Remove-Job
+    $session | Remove-PSSession
+
+}
