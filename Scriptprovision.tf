06c8b15ea15e9414dcfe11bc26269e5fdc2b8c74
Fededaher <felipequeiroz05@gmail.com>
2024-03-04 15:45:24 -0300

    Update Scriptprovision.tf
    corrigido

--- Scriptprovision.tf
+++ Scriptprovision.tf
@@ -32,7 +32,7 @@ resource "azurerm_virtual_network" "vnet" {
 resource "azurerm_subnet" "sub01" {
   name                 = "sub-prd01"
   resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vnet01.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/16"]
 }
 
