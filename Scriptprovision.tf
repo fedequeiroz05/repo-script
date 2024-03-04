8ca487730d99ecee021770cb2b7cf503dcd6fb38
Fededaher <felipequeiroz05@gmail.com>
2024-03-04 15:44:25 -0300

    Update Scriptprovision.tf
    Adicionado subnet

--- Scriptprovision.tf
+++ Scriptprovision.tf
@@ -21,9 +21,18 @@ resource "azurerm_resource_group" "rg-tf" {
 
 # create a virtual network within the resource group
 
-resource "azurerm_virtual_network" "vnet-win" {
+resource "azurerm_virtual_network" "vnet" {
   name = "vnet-win"
   resource_group_name = azurerm_resource_group.rg-tf
   location = azurerm_resource_group.eastus
   address_space = ["10.0.0.0/16"]  
 }
+
+# Deploy subnet 
+resource "azurerm_subnet" "sub01" {
+  name                 = "sub-prd01"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet01.name
+  address_prefixes     = ["10.0.1.0/16"]
+}
+
